2c82d8e3155694864c21f53c68dc19e4ee2cfccb
Halal37 <wojciechlidwin37@gmail.com>
2022-05-30 20:56:04 +0200

    Create Basic Concepts .java
    Second and third lesson of the Java Sololearn course

---
+++ Java/Basic Concepts .java
@@ -0,0 +1,42 @@
+//2 Lesson
+
+//The main Method
+//Fill in the blank to declare a method called "hello()".
+void hello();
+//Which method prints text in a Java program?
+System.out.println();
+//Fill in the blanks to create a valid Java program.
+class
+ Apples {
+
+    public static void 
+main
+(String[ ]args) {
+
+    System.out.println("Hello, World!");
+
+   }
+
+}
+
+//3 Lesson
+
+//Single-line comments are created using:
+// characters at the beginning of the line
+
+//Make this text a multi-line comment.
+/*
+ some
+
+* comment text
+
+*/
+
+//You can add a Java doc style comment by using:
+/** and */ characters to wrap a comment.
+
+
+
+
+
+
