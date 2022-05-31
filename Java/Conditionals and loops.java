ace7917803bdfd1c989b0f17edcc7604e62d0184
Halal37 <wojciechlidwin37@gmail.com>
2022-05-31 15:04:14 +0200

    Create Conditionals and loops.java
    11 and 12 lesson of the Java Sololearn course

---
+++ Java/Conditionals and loops.java
@@ -0,0 +1,39 @@
+//11 Lesson
+
+//Fill in the blanks to print "Yeah".
+int x = 5;
+if ( x == 5 ) {
+System.out.println("Yeah");
+}
+
+//Fill in the blanks to print the greater number.
+
+int x = 10;  int y = 5;
+
+if( x > y ) {
+
+  System.out.println(x);
+}
+else{
+
+  System.out.println(y);
+
+}
+
+//12 Lesson
+
+//Please fill in the missing parts of the nested if statement to print "it works!" to the screen.
+int x = 37;
+if (x > 22) {
+
+if(x > 31) {
+
+ System.out.println("it works!");
+
+ }
+
+}
+
+//13 Lesson
+
+
