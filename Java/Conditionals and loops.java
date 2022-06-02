8d50a736294de273c1d5c44d38d81daea3e67865
Halal37 <wojciechlidwin37@gmail.com>
2022-06-02 20:50:22 +0200

    Update Conditionals and loops.java
    17,18 and 19 lesson of the Java Sololearn course

--- Java/Conditionals and loops.java
+++ Java/Conditionals and loops.java
@@ -135,3 +135,79 @@ while (x < y) {
 //5
 
 //17 Lesson
+
+//Drag and drop from the options below to print "Great!" 10 times:
+
+for (int i = 0; i < 10; i++) {
+
+   System.out.println("Great!");
+
+
+}
+
+//How many times will the following loop run?
+//for (int i = 2; i < 10; i = i*i) {
+//   System.out.println(i);
+//}
+//2
+
+//18 Lesson
+
+//How is a do while loop different from a while loop?
+
+//A do while loop runs your code at least one time.
+
+//Fill in the blanks to print the values of the x variable 5 times.
+
+
+int x = 1;
+
+do {
+
+ System.out.println(x);
+
+ x++;
+
+} 
+
+while(x <= 5);
+
+//19 Lesson
+
+//Fill in the blanks to print "in a loop" 7 times, using the while loop.
+int x = 1;
+
+while (x <= 7) {
+
+  System.out.println("in a loop");
+  
+x++;
+
+}
+
+//Please select the correct statements about && and || operators.
+
+// a || b is true if either a or b is true
+//( a || b) && c is true if c is true and either a or b is true
+
+//Fill in the blanks to print "You rock!" if variable "a" is greater than 15, and variable "b" is less than or equal to 72.
+int a = 144;
+
+int b = 33;
+
+if (a > 15 &&b <= 72) {
+
+  System.out.println("You rock!");
+
+}
+
+//Fill in the blanks to print "in a loop" 5 times using the for loop.
+for
+ (int x = 0; x < 5; x++) {
+
+  System.out.println("in a loop");
+
+}
+
+
+//20 Lesson
