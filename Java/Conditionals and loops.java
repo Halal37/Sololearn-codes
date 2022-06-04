f93bf3d62bb31ac09eb4b028ed54d84af7d5483d
Halal37 <wojciechlidwin37@gmail.com>
2022-06-05 00:12:59 +0200

    Update Conditionals and loops.java
    20 lesson of the Java Sololearn course

--- Java/Conditionals and loops.java
+++ Java/Conditionals and loops.java
@@ -211,3 +211,43 @@ for
 
 
 //20 Lesson
+
+/*
+Loan Calculator
+
+You take a loan from a friend and need to calculate how much you will owe him after 3 months.
+You are going to pay him back 10% of the remaining loan amount each month.
+Create a program that takes the loan amount as input, calculates and outputs the remaining amount after 3 months.
+
+Sample Input:
+20000
+
+Sample Output:
+10628
+
+Here is the monthly payment schedule:
+Month 1
+Payment: 10% of 20000 = 2000
+Remaining amount: 18000
+Month 2
+Payment: 10% of 18000 = 1800
+Remaining amount: 16200
+Month 3:
+Payment: 10% of 16200 = 1620
+Remaining amount: 14580
+*/
+
+import java.util.Scanner;
+
+public class Program
+{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int amount = scanner.nextInt();
+		int loan = 20000;
+		for (int i=0;i<=2;i++){
+			loan = loan - loan * 1/10;
+		}
+		System.out.println(loan);
+	}
+}
