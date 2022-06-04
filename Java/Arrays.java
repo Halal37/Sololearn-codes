2b0d42233876e04cf60c84646f59b7d6e0d60697
Halal37 <wojciechlidwin37@gmail.com>
2022-06-05 00:19:06 +0200

    Create Arrays.java
    21 lesson of the Java Sololearn course

---
+++ Java/Arrays.java
@@ -0,0 +1,14 @@
+// 21 Lesson
+
+//In Java, an array starts counting from...
+
+//0
+
+
+//Initializing Arrays
+//What is the output of this code?
+//int tmp[ ] = {5, 8, 9, 3};
+//System.out.println(tmp[2]);
+
+//9
+
