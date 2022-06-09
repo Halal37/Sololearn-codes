b7a86edbefb3c7b8c7c363b8f517fa2326ba9fe4
Halal37 <wojciechlidwin37@gmail.com>
2022-06-10 00:08:33 +0200

    Update Exceptions, Lists, Threads & Files.java
    56 and 57 lessons of the Java Sololearn course

--- Java/Exceptions, Lists, Threads & Files.java
+++ Java/Exceptions, Lists, Threads & Files.java
@@ -1,6 +1,53 @@
 // 56 Lesson
 
+//Which two of the following statements are true?
+//Select All That Apply
+
+// If we don't catch exceptions, the program can shut down
+
+// Exceptions catching is a good practice
+
+
+//Fill in the blanks to handle any possible exceptions.
+int x = 12;
+
+int y = 0;
+
+try {
+
+   int z = x / y;
+
+   System.out.println(z);
+
+}
+
+catch (Exception e) {
+
+   System.out.println("Error");
+
+}
+
+
 // 57 Lesson
+
+//Drag and drop from the options below to create a method that throws an IOException if the parameter is negative.
+public void do(int x)
+  
+throws IOException {
+
+  if(x<0)
+
+  {
+    
+ throw new IOException();
+
+  }
+
+}
+
+//How many catch blocks can a try/catch block contain?
+
+//As many as you need
 
 // 58 Lesson
 
