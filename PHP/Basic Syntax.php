4c598c1972f80322dac0dd944717ede51682b661
Halal37 <wojciechlidwin37@gmail.com>
2022-06-10 21:47:17 +0200

    Create Basic Syntax.php
    1,2,3,4 and 5 lessons of the PHP Sololearn course

---
+++ PHP/Basic Syntax.php
@@ -0,0 +1,87 @@
+<?php
+// 1 Lesson
+
+//PHP is a...
+
+//Server side programming language
+
+
+//Can you run PHP on Linux?
+
+//Yes
+
+// 2 Lesson
+
+//Fill in the correct character.
+<?php
+
+  echo 'Hello';
+
+?>
+
+//Which of the following is correct when using PHP with the script tag?
+
+//<script language="php">
+
+//Which is the most widely recommended way to use PHP tags?
+
+//<?php
+
+// 3 Lesson
+
+//Fill in the blanks to output "Hi" using echo:
+<?php
+
+   echo "Hi";
+
+?>
+
+//Which character has to be at the end of each statement?
+
+//;
+
+//Fill in the blanks to output "SoloLearn" as a h1 heading:
+<?php
+
+  echo "<h1>SoloLearn</h1>";
+
+?>
+
+// 4 Lesson
+
+//Which of the following characters indicates comments in PHP?
+
+// //
+
+//Fill in the blanks to make the text a comment.
+
+/*
+ Name: John
+
+    Age : 24
+
+    Date: 05/06/2014
+
+*/
+
+// 5 Lesson
+
+//Which of the following is the correct syntax?
+
+//echo "<p> Hello.</p>";
+
+//Fill in the blank to make the text a comment:
+
+// This is a comment
+
+echo 'This is a test';
+
+//Drag and drop from the options below to form the correct PHP code:
+
+<?php  
+
+ echo "Some text";
+
+?>
+
+?>
