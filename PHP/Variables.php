1fb66d166514e64469c68504b75299980187e9af
Halal37 <wojciechlidwin37@gmail.com>
2022-06-21 18:59:28 +0200

    Update Variables.php
    6, 7, 8, 9, 10 and 11 lessons of the PHP Sololearn course

---
+++ PHP/Variables.php
@@ -0,0 +1,100 @@
+<?php
+// 6 Lesson
+
+//..Fill in the blanks to declare the variables $name and $age and assign their values.
+<?php
+
+  
+$name = "David";
+
+  $age = 25;
+
+?>
+
+// 7 Lesson
+
+//Fill in the blanks to declare a case-sensitive constant called AGE and assign the value 28 to it.
+<?php
+  
+define("AGE", 28);
+
+?>
+
+// 8 Lesson
+
+//Fill in the blank to define the variable string:
+<?php
+  
+$string = "I am learning PHP";
+
+?>
+
+/*
+What output results from the following code?
+<?php
+ $someString = "15";
+ $birthyear = 1982;
+ echo $someString + $birthyear;
+?>
+*/
+
+//1997
+
+// 9 Lesson
+
+//In the example, why did we get a PHP error?
+
+//The variable was not defined within the function
+
+/*
+What output results from the following code?
+<?php 
+$num1 = 56;
+function  my_func() {
+  $num1 = 89;
+  echo $num1;
+}
+my_func();
+?>
+*/
+
+//89
+
+// 10 Lesson
+
+//Fill in the blank to print ''World'' to the screen.
+<?php 
+
+  $Hello = "World";
+
+  $a = "Hello";
+
+  echo $$a; 
+
+?>
+
+// 11 Lesson
+
+//Drag and drop from the options below to define a variable called 'name', assign it a value, and print it to the screen.
+$name = "Robert";
+
+echo $name;
+
+/*
+What output results from the following code?
+$x = 'y';
+$y = 'x';
+echo $$x;
+*/
+
+//x
+
+
+//Rearrange the code to define a variable and print it's value.
+
+<?php
+$test = 42;
+echo $test;
+?>
+
+?>
