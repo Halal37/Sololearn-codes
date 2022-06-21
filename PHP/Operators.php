4ae70e95ff0db5e7afb408de0628226a99f26b33
Halal37 <wojciechlidwin37@gmail.com>
2022-06-21 19:15:52 +0200

    Update Operators.php
    12, 13, 14, 15 and 16 lessons of the PHP Sololearn course

---
+++ PHP/Operators.php
@@ -0,0 +1,88 @@
+<?php
+
+// 12 Lesson
+
+//Fill in the blanks to subtract one number from the other.
+<?php
+
+$num1 = 23;
+
+$num2 = 16;
+
+echo $num1 - $num2;
+
+?>
+
+/*
+What would the value of the variable $num be after the following code?
+<?php
+$num = 9;
+$num = $num%2; 
+?>
+*/
+
+//1
+
+/*
+What output results from the following code?
+<?php  
+$num = 18; 
+$num--;
+echo $num;
+?>
+*/
+
+//17
+
+// 13 Lesson
+
+/*
+What output results from the following code?
+<?php 
+$a = 7; $b = 3; 
+$a += $b;
+echo $a;
+?>
+*/
+
+//10
+
+// 14 Lesson 
+
+//Which of the following corresponds to the not equal to operator?
+
+//!=
+
+//Which of the following returns TRUE, if $num1=4; $num2=8?
+
+//$num1 != $num2;
+
+// 15 Lesson
+
+/*
+What is the return value of the following expression, if $num1 = 5 and $num2 = 3?
+($num1 >1 && $num2 < $num1)
+*/
+
+//TRUE
+
+// 16 Lesson
+
+//Which symbol corresponds to the logical OR operator?
+
+//||
+
+//Rearrange the code to define the variable 'age', increment it by one, and print it to the screen.
+
+<?php
+$age = 21;
+$age++;
+echo $age;
+?>
+
+//What is the value of $b?
+//$a=2; $b=$a++;
+
+//2
+
+?>
