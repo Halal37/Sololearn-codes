c83da0403aebdde7414f5f8d19e51713276caad1
Halal37 <wojciechlidwin37@gmail.com>
2022-06-22 18:23:12 +0200

    Create Basic Concepts.py
    1, 2, 3 and 4 lessons of the Python Sololearn course

---
+++ Python/Basic Concepts.py
@@ -0,0 +1,73 @@
+# 1 Lesson
+
+#Python is a:
+
+#Programming language
+
+#Drag & drop the correct option to output "Hi".
+
+print("Hi")
+
+#Rearrange the code to generate the following output:
+#A
+#B
+#C
+
+print("A")
+print("B")
+print("C")
+
+#Fill in the required parentheses to create a valid print statement.
+
+print("I love Python")
+
+# 2 Lesson
+
+"""
+What does this code output?
+
+print(1 + 2 + 3)
+"""
+
+#6
+
+"""
+Which option is output by this code?
+
+print((4 + 8) / 2)
+"""
+
+#6.0
+
+# 3 Lesson
+
+#Which of these will not be stored as a float?
+
+#7
+
+"""
+Fill in the blank with the output of this code.
+
+print(1 + 2 + 3 + 4.0 + 5)
+"""
+
+#15.0
+
+# 4 Lesson
+
+#Fill in the blank to output 5 raised to the 3rd power.
+
+print(5**3)
+
+"""
+What is the output of this code?
+print( 8 ** (1/3) )
+"""
+
+#2.0
+
+# 5 Lesson
+
+# 6 Lesson
+
+# 7 Lesson
