b12272c2940fce3d20c2ea2d1ef4f53181f9bbc0
Halal37 <wojciechlidwin37@gmail.com>
2022-06-23 13:03:18 +0200

    Update Basic Concepts.py
    5, 6 and 7 lessons of the Python Sololearn course

--- Python/Basic Concepts.py
+++ Python/Basic Concepts.py
@@ -68,6 +68,68 @@ print( 8 ** (1/3) )
 
 # 5 Lesson
 
+"""
+What is the output of this code?
+
+print( 10//4 )
+"""
+
+#2
+
+"""
+What is the result of this code?
+
+print( 7%(5 // 2) )
+"""
+
+#1
+
 # 6 Lesson
 
+"""
+What is the result of the following operation?
+print(1 + 4*3)
+"""
+
+#13
+
+#Fill in the blanks to output "I love Python".
+
+print("I love Python")
+
+"""
+Guess the output of this code:
+print( (3**2)//2 )
+"""
+
+#4
+
+#Which of the following operators is used to calculate the remainder of division?
+
+#%
+
+#Fill in the blank to output the quotient of dividing 100 by 42.
+
+print(100//42)
+
+#Drag & drop from the options below to produce code that output 10 raised to the 5th power.
+
+print(10**5)
+
 # 7 Lesson
+"""
+Exponentiation is the raising of one number to the power of another.
+This operation is performed using two asterisks **.
+
+Let's use exponentiation to solve a known problem.
+You are offered a choice of either $1.000.000 or $0.01 (one penny) doubled every day for 30 days (the resulting amount is doubled every day).
+
+Task:
+Write a program to calculate the amount that will result from the doubling to understand which choice results in a larger amount.
+
+Hint:
+Let's see how exponentiation can be useful to perform the calculation.
+For example, if we want to calculate how much money we will have on the 5th day, we can use this expression: 0.01*(2**5) = 0.32 dollars (multiply the penny by 2 raised to the power of 5).
+"""
+
+print(0.01*(2**30))
