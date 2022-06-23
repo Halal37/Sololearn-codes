5ebc448179b5f0bee2333f6f5f8292670ae74540
Halal37 <wojciechlidwin37@gmail.com>
2022-06-23 13:44:44 +0200

    Update Pythonicness & Packaging.py
    8, 9, 10 and 11 lessons of the Python Sololearn course

--- Python/Pythonicness & Packaging.py
+++ Python/Pythonicness & Packaging.py
@@ -1,10 +1,52 @@
 # 92 Lesson
 
+#Complete the code to output “Hello world!”.
+
+print("Hello world!")
+
+#Complete the code to output a string containing a single quote.
+
+print('I\'m learning!')
+
 # 93 Lesson
+
+#Which of the following options results in exactly two lines?
+
+#"Hello \n world"
+
+"""
+How many lines will the following code output?
+print("Hi")
+print("""This 
+is
+great""")
+"""
+
+#4
 
 # 94 Lesson
 
+#Fill in the blanks to produce “Python is awesome”.
+
+print("Python" + "is" + "awsome")
+
+"""
+What is the output of this code?
+
+print(3 * '7')
+"""
+
+#777
+
 # 95 Lesson
+
+#Fill in the blanks to create a variable "age" and assign the value 42 to it:
+
+age = 42
+
+#Which of these is a valid variable name in Python?
+
+#A_VARIABLE_NAME
 
 # 96 Lesson
 
