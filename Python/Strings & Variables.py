032ac0996205ee10909449d5c4a05993a8a53c46
Halal37 <wojciechlidwin37@gmail.com>
2022-06-23 13:52:45 +0200

    Update Strings & Variables.py
    8, 9, 10 and 11 lessons of the Python Sololearn course

--- Python/Strings & Variables.py
+++ Python/Strings & Variables.py
@@ -1,10 +1,52 @@
 # 8 Lesson
 
+#Complete the code to output “Hello world!”.
+
+print("Hello world!")
+
+#Complete the code to output a string containing a single quote.
+
+print('I\'m learning!')
+
 # 9 Lesson
+
+#Which of the following options results in exactly two lines?
+
+#"Hello \n world"
+
+"""
+How many lines will the following code output?
+print("Hi")
+print("""This 
+is
+great""")
+"""
+
+#4
 
 # 10 Lesson
 
+#Fill in the blanks to produce “Python is awesome”.
+
+print("Python" + "is" + "awsome")
+
+"""
+What is the output of this code?
+
+print(3 * '7')
+"""
+
+#777
+
 # 11 Lesson
+
+#Fill in the blanks to create a variable "age" and assign the value 42 to it:
+
+age = 42
+
+#Which of these is a valid variable name in Python?
+
+#A_VARIABLE_NAME
 
 # 12 Lesson
 
