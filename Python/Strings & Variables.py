01222ec8bd9854b8737650e3bea6e7041f005b63
Halal37 <wojciechlidwin37@gmail.com>
2022-06-24 15:45:41 +0200

    Update Strings & Variables.py
    12, 13, 14, 15, 16 and 17 lessons of the Python Sololearn course

--- Python/Strings & Variables.py
+++ Python/Strings & Variables.py
@@ -50,12 +50,146 @@ age = 42
 
 # 12 Lesson
 
+"""
+What is the output of this code?
+spam = "eggs"
+print(spam * 3)
+"""
+
+#eggseggseggs
+
+#Drag the correct answer to output 12.
+x = 5
+y = 7
+print(x + y)
+
 # 13 Lesson
+
+#Fill in the blank to take user input, assign it to a variable, and output it:
+
+text = input()
+
+print(text)
+
+#Fill in the blanks to prompt the user with a message and assign the input to the variable "name":
+
+name = input("What is your name?")
 
 # 14 Lesson
 
+"""
+What will be the output of this code?
+
+x = “2”
+y = “4”
+z = int(x) + int(y)
+print(z)
+"""
+
+#6
+
+#Fill in the blanks to produce a valid code, which takes the values of the x and y variables from the input and outputs their sum.
+x = int(input())
+
+y = int(input())
+
+print(x+y)
+
 # 15 Lesson
+
+"""
+What is the output of this code?
+
+x = 4
+x *= 3
+print(x)
+"""
+
+#12
+
+"""
+What is the result of this code?
+
+x = "a"
+x *= 3
+print(x)
+"""
+
+#aaa
+
+#Fill in the blanks to produce code which takes text as input, assigns it to the name variable, and outputs it.
+
+print(name:=input())
 
 # 16 Lesson
 
+"""
+What is the output of this code?
+
+spam = "7"
+spam = spam + "0"
+eggs = int(spam) + 3
+print(float(eggs))
+"""
+
+#73.0
+
+"""
+What is the output of this code if the user enter '42' as input:
+age = int(input())
+print(age+8)
+"""
+
+#50
+
+"""
+What is the output of this code?
+
+ x = 5
+ y = x + 3
+ y = int(str(y) + "2")
+ print(y)
+"""
+
+#82
+
+#Fill in the blanks to declare a variable, add 5 to it and print its value.
+
+x = 4
+
+x += 5
+
+print(x)
+
+"""
+What is the output of this code?
+
+x = 3
+num = 17
+print(num % x)
+"""
+
+#2
+
+#Fill in the blanks to take a name as input and output a Welcome message, containing the input?
+name = input()
+
+print("Welcome, "+ name)
+
 # 17 Lesson
+
+"""
+Write a program to take two integers as input and output their sum.
+
+Sample Input:
+2
+8
+
+Sample Output:
+10
+"""
+
+x=int(input())
+y=int(input())
+x+=y
+print(x)
