1288f51f374230f06809cb14b506bbf447f74da2
Halal37 <wojciechlidwin37@gmail.com>
2022-06-25 20:29:26 +0200

    Update Control Structures.py
    18, 19 and 20 lessons of the Python Sololearn course

--- Python/Control Structures.py
+++ Python/Control Structures.py
@@ -1,8 +1,96 @@
 # 18 Lesson
 
+#What are the two Boolean values in Python?
+
+#True and False
+
+"""
+What is the output of this code?
+
+print(7 != 8)
+"""
+
+#True
+
+"""
+What is the output of this code?
+
+print( 7 > 7.0 )
+"""
+
+#False
+
+"""
+What is the output of this code?
+
+print( 8.7 <= 8.70 )
+"""
+
+#True
+
 # 19 Lesson
 
+#What part of an if statement should be indented?
+
+#The statements within it
+
+"""
+What is the output of this code?
+
+spam = 7
+if spam > 5:
+   print("five")
+if spam > 8:
+   print("eight")
+"""
+
+#five
+
+"""
+What is the output of this code?
+
+num = 7
+if num > 3:
+   print("3")
+   if num < 5:
+      print("5")
+      if num ==7:
+         print("7")
+"""
+
+#3
+
 # 20 Lesson
+
+"""
+What is the result of this code?
+
+if 1 + 1 == 2:
+   if 2 * 2 == 8:
+      print("if")
+   else:
+      print("else")
+"""
+
+#else
+
+#Fill in the blanks to compare the variables and output the corresponding text:
+x = 10
+
+y = 20
+
+if x > y:
+
+
+   print("if statement")
+
+else:
+
+   print("else statement")
+   
+#A shorter option for an "else if" statement is:
+
+#elif  
 
 # 21 Lesson
 
