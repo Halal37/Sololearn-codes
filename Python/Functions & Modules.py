82ed34aa8c4c5ba49b1fb51db78d3bc766882193
Halal37 <wojciechlidwin37@gmail.com>
2022-07-01 16:55:30 +0200

    Update Functions & Modules.py
    31, 32 and 33 lessons of the Python Sololearn course

--- Python/Functions & Modules.py
+++ Python/Functions & Modules.py
@@ -1,8 +1,55 @@
 # 31 Lesson
 
+#Following the DRY principle makes the code:
+
+#easier to maintain
+
+#How many arguments are in this function call?
+#range(0, 100, 5)
+
+#3
+
 # 32 Lesson
 
+#Fill in the blanks to define a function named hello:
+def hello():
+
+  print("Hi!")
+
+#Rearrange the code to declare a function sayHi() and call it.
+
+def sayHi():
+ print("Hi!")
+sayHi()
+
 # 33 Lesson
+
+"""
+What is the result of this code?
+def print_double(x):
+   print(2 * x)
+
+print_double(3)
+"""
+
+#6
+
+#Fill in the blanks to define a function that takes two arguments and prints their multiplication.
+def print_mult(x, y):
+
+   print(x * y)
+
+#Fill in the blanks to define a function that prints "Yes", if its parameter is an even number, and "No" otherwise.
+def
+ even(x):
+
+   if x%2 == 0:
+
+     print("Yes")
+
+   else:
+
+     print("No")
 
 # 34 Lesson
 
