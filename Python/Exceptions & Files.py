a9ebe535b847db2b4c7bbb89841f262fb25e6509
Halal37 <59863785+Halal37@users.noreply.github.com>
2022-07-20 01:59:00 +0200

    Update Exceptions & Files.py
    41, 42 and 43 lessons of the Python Sololearn course

--- Python/Exceptions & Files.py
+++ Python/Exceptions & Files.py
@@ -1,8 +1,102 @@
 # 41 Lesson
 
+#What is an exception?
+
+#An event that occurs due to incorrect code or input
+
+#Which exception is raised by this code?
+
+#print("7" + 4)
+
+#TypeError
+
 # 42 Lesson
 
+"""
+What is the output of this code?
+
+try:
+
+  variable = 10
+
+  print (10 / 2)
+
+except ZeroDivisionError:
+
+  print("Error")
+
+print("Finished")
+"""
+
+#5.0 Finished
+
+"""
+What is the output of this code?
+
+try:
+
+  meaning = 42
+
+  print(meaning / 0)
+
+  print("the meaning of life")
+
+except (ValueError, TypeError):
+
+  print("ValueError or TypeError occurred")
+
+except ZeroDivisionError:
+
+  print("Divided by zero")
+"""
+
+#Divided by zero
+
+#Fill in the blanks to handle all possible exceptions.
+
+ try:
+  num1 = input(":")
+
+  num2 = input(":")
+
+  print(float(num1)/float(num2))
+
+ except:
+  print("Invalid input")
+
 # 43 Lesson
+
+"""
+What is the output of this code?
+
+try:
+
+  print(1)
+
+except:
+
+  print(2)
+
+finally:
+
+  print(3)
+"""
+
+#1 3
+
+#Drag and drop from the options below to create a try/except/finally block.
+
+try:
+
+  print(1)
+
+except:
+
+  print(2)
+
+finally:
+
+  print(42)
 
 # 44 Lesson
 
