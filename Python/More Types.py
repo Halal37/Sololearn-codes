6eaf251048ba1ae8cd97c40f57deb335c7bbca05
Halal37 <59863785+Halal37@users.noreply.github.com>
2022-08-03 19:46:28 +0200

    Update More Types.py
    52, 53, 54, 55, 56 and 57 lessons of the Python Sololearn course

--- Python/More Types.py
+++ Python/More Types.py
@@ -1,14 +1,152 @@
 # 52 Lesson
 
+#What is "None" often used to represent?
+
+#Absence of a value
+
+"""
+What number does this code print?
+
+foo = print()
+
+if foo == None:
+
+   print(1)
+
+else:
+
+   print(2)
+
+"""
+
+#1
+
 # 53 Lesson
+
+#Fill in the blanks to define a valid dictionary with two elements.
+
+cars = {"BMW": "blue", "Volvo": "red"}
+
+"""
+What is the result of this code?
+
+test = { }
+
+print(test[0])
+"""
+
+#KeyError
+
+#Which of these values can't be used as a dictionary key?
+
+{2: 4, 3: 9, 4: 16,}
 
 # 54 Lesson
 
+"""
+What is the result of this code?
+
+primes = {1: 2, 2: 3, 4: 7, 7:17}
+
+print(primes[primes[4]])
+
+"""
+
+#17
+
+#Drag and drop from the options below to print "Yes", if the key 112 is present in the dictionary named "pairs".
+
+if 112 in pairs:
+
+  print("Yes")
+
+"""
+What is the result of this code?
+
+fib = {1: 1, 2: 1, 3: 2, 4: 3}
+
+print(fib.get(4, 0) + fib.get(7, 5))
+"""
+
+#8
+
 # 55 Lesson
+
+#Fill in the blanks to create a list, dictionary, and tuple:
+
+# list
+
+list = ["one", "two"]
+
+# dictionary 
+
+dict = {1:"one", 2:"two"}
+
+# tuple 
+
+tp = ("one", "two")
+
+"""
+What is the result of this code?
+
+tuple = (1, (1, 2, 3))
+
+print(tuple[1])
+"""
+
+#(1, 2, 3)
 
 # 56 Lesson
 
+#What is the result of this code?
+
+sqs = [0, 1, 4, 9, 16, 25, 36, 49, 64]
+
+#[16, 25, 36]
+
+#Fill in the blanks to take the first two elements of the list:
+
+list = ["a", "b", "c", "d"]
+
+a = list[0:2]
+
+"""
+What is the output of this code?
+
+sqs = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+
+print(sqs[1::4])
+"""
+
+#[1, 25, 81]
+
+"""
+What is the output of this code?
+
+sqs = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+
+print(sqs[7:5:-1])
+"""
+
+#[49, 36]
+
 # 57 Lesson
+
+"""
+What does this list comprehension create?
+
+nums = [i*2 for i in range(10)]
+"""
+
+#A list of even numbers between 0 and 18
+
+#Create a list of multiples of 3 from 0 to 20.
+
+a = [i for i in range(20) if i%3==0]
+
+#Fill in the blanks to create a list of numbers multiplied by 10 in the range of 5 to 9.
+
+a = [x*10 for x in range(5, 9)]
 
 # 58 Lesson
 
