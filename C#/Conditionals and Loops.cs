261140fc54fc4dd3af3195655bab1691a93098de
Halal37 <59863785+Halal37@users.noreply.github.com>
2022-08-14 19:15:44 +0200

    Update Conditionals and Loops.cs
    13 and 14 lessons of the C# Sololearn course

--- C#/Conditionals and Loops.cs
+++ C#/Conditionals and Loops.cs
@@ -1,6 +1,115 @@
 // 13 Lesson
 
+//Fill in the blanks to display Welcome to the screen when age is greater than 16.
+
+int age = 24;
+
+if(age > 16) 
+
+{
+
+   Console.WriteLine("Welcome");
+}
+
+//Which is the correct operator for equality testing?
+
+//==
+
+//Fill in the blanks to find the larger of two variables.
+
+int a = 42;
+
+int b = 88;
+
+if(a > b)
+
+{
+
+   Console.WriteLine(a);
+
+} 
+
+else{
+
+   Console.WriteLine(b);
+
+}
+
+/*
+What is the output of this code?
+int a = 8;
+int b = ++a;
+if(a > 5)
+   b -= 3;
+else
+   b = 9;
+Console.WriteLine(b);
+*/
+
+//6
+
+//How many nested if statements can an if statement contain?
+
+//As many as you want
+
+
 // 14 Lesson
+
+//Fill in the blanks to create a valid switch statement.
+int x = 33;
+
+switch(x) {
+
+  case 8:
+
+    Console.WriteLine("Value is 8");
+
+    break;
+
+  case 18:
+
+
+    Console.WriteLine("Value is 18");
+
+    break;
+
+  
+case 33:
+
+    Console.WriteLine("Value is 33");
+
+    break;
+
+}
+
+//Drag and drop from the options below to test the x variable with a switch statement.
+
+switch (x) {
+
+  case 10:
+
+    Console.WriteLine("Ten");
+
+    break;
+
+  case 20:
+
+    Console.WriteLine("Twenty");
+
+    break;
+
+  default:
+
+    Console.WriteLine("No match");.
+
+    break;
+
+}
+
+//What would occur if we forget to include a break statement at the end of case code?
+
+//compile error
+
 
 // 15 Lesson
 
