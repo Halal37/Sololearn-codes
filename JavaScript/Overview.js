7a695f1472d3a3aef068873801096b3e7b73e0e6
Halal37 <59863785+Halal37@users.noreply.github.com>
2022-09-04 15:39:12 +0200

    Update Overview.js
    1, 2, 3, 4, 5 and 6 lessons of the JavaScript Sololearn course

--- JavaScript/Overview.js
+++ JavaScript/Overview.js
@@ -1,11 +1,118 @@
 // 1 Lesson
 
+//What is JavaScript?
+
+//a very popular programming language
+
+//True or false? You can use the console to test code and fix bugs.
+
+//True
+
+//Which command is used to log or write messages to the console?
+
+//console.log()
+
+//Drag and drop to send the message to the console.
+
+console.log('Testing')
+
+//Drag and drop to send the message 'Error' to the console
+
+console.log('Error')
+
+//Select the correct way to send 'Lesson Completed!' to the console
+
+//console.log('Lesson Completed!')
+
 // 2 Lesson
+
+//Output "Hello!" in the browser.
+<script>
+
+document.write("Hello!");
+
+</script>
+
+//Complete the code to output "Hi!" to the console.
+
+console.log("Hi!")
 
 // 3 Lesson
 
+//How do we tell JavaScript that we're working with a variable?
+
+//var
+
+//Choose the correct keyword to declare a variable and assign the value of 32.
+
+var my_variable = 32;
+
+//Which of these characters can we use to start a variable?
+//Select All That Apply
+
+// Underscore sign (_)
+// Letters
+
 // 4 Lesson
+
+//What does a single line comment look like?
+
+// this is a comment
+
+//Create a multi-line comment in JavaScript.
+
+/*
+ this is a
+
+multiline
+
+comment 
+*/
 
 // 5 Lesson
 
+//Fill in the blanks to declare a variable age and assign it the number 18:
+
+var age = 18;
+
+//To create a string, we need to put the text inside…
+
+//Quotation marks
+
+//Which of the following is the escape character?
+
+\
+
+//Which two values does the Boolean data type accept?
+//Select All That Apply
+
+//false
+//true
+
 // 6 Lesson
+
+//Fill in the blanks to output "JS is cool!" to the console:
+
+console.log("JS is cool!");
+
+//Declare a variable called x, assign the value 42 to it and output it to the console.
+
+var x = 42;
+console.log(x);
+
+/*
+What is the output of this code?
+//x=8;
+x=2;
+//x=3;
+console.log(x);
+*/
+
+//2
+
+//Rearrange to form valid JavaScript code that declares a variable and outputs it to the console.
+
+<script>
+var name = "James";
+console.log(name);
+</script>
